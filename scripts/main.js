79deb17a19a97028db833cadd0a4b901934889d4
YumProjects <32969785+ben-hess@users.noreply.github.com>
2022-02-23 16:59:27 -0800

    Added dummy bot

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
 var player;
 var wheel;
+var hand;
+
+var dummy;
+
+setTimeout(function() { alert("|    <----- STICK"); }, 86400000);
+
 
 runOnStartup(async runtime =>
 {
@@ -11,6 +17,8 @@ async function OnBeforeProjectStart(runtime)
 {
 	player = runtime.getInstanceByUid(2);
 	wheel = runtime.getInstanceByUid(8);
+	hand = runtime.getInstanceByUid(9);
+	dummy = runtime.getInstanceByUid(10);
 	runtime.addEventListener("tick", () => Tick(runtime));
 }
 
@@ -20,13 +28,20 @@ function Tick(runtime)
 	runtime.layout.scrollY += (player.y - runtime.layout.scrollY) * 5 * runtime.dt;
 	wheel.x = player.x;
 	wheel.y = player.y;
+	hand.x = player.x + player.behaviors.Platform.vectorX / 100;
+	hand.y = player.y;
 	wheel.angle += player.behaviors.Platform.vectorX / 2000;
-	player.angle = player.behaviors.Platform.vectorX / 1000;
+	player.angle = player.behaviors.Platform.vectorX / 2000;
+	
+	dummy.x += ((player.x + 100) - dummy.x) / 50;
+	dummy.y += ((player.y - 50) - dummy.y) / 50 + Math.sin(runtime.gameTime * 2) * 0.5;
 	
 	if(player.behaviors.Platform.vectorX > 0){
 		player.width = 62.5;
+		hand.width = 17;
 	}
 	else if(player.behaviors.Platform.vectorX < 0){
 		player.width = -62.5;
+		hand.width = -17;
 	}
 }
